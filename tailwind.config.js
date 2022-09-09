bad7ab42dd88255ef128b175909c659d24492628
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-09-09 15:35:38 -0300

    Code relinked

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*{html,js}"],
+  content: ["./index.html","./src/index.js"],
   theme: {
     extend: {
       colors:{
